b42b14185d5e76818f1c67cd0578a11b98d9188b
Sorraimi Rivas <sorraimi.rivas@hotmail.com>
2023-08-14 19:29:52 -0400

    add: nativewind for styling

--- App.tsx
+++ App.tsx
@@ -1,23 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import HomeScreen from "./screen/timer/TimerScreen";
-import TimerSettingsScreen from "./screen/timer/TimerSettingsScreen";
+import { View } from "react-native";
+import TimerSettingsScreen from "./src/screens/timer/TimerSettingsScreen";
+
+const color = "#20B2AA";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <HomeScreen /> */}
+    <View className="bg-white">
       <TimerSettingsScreen />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#20B2AA",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
