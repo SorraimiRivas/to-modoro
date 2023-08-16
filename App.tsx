fce09aacb056f1465191a5107e3f799af76c0bb1
Sorraimi Rivas <sorraimi.rivas@hotmail.com>
2023-08-16 18:50:26 -0400

    add: expo fonts for topography

--- App.tsx
+++ App.tsx
@@ -1,10 +1,33 @@
+import { useCallback } from "react";
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import TimerScreen from "./src/screens/timer/TimerScreen";
 import TimerSettingsScreen from "./src/screens/timer/TimerSettingsScreen";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    "Merriweather-Light": require("./src/assets/fonts/Merriweather-Light.ttf"),
+    "Poppins-Black": require("./src/assets/fonts/Poppins-Black.ttf"),
+    Poppins: require("./src/assets/fonts/Poppins-Regular.ttf"),
+    "Roboto-Regular": require("./src/assets/fonts/Roboto-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded || fontError) {
+    return null;
+  }
+
   return (
-    <View className="bg-white">
+    <View className="" onLayout={onLayoutRootView}>
       <TimerSettingsScreen />
       <StatusBar style="auto" />
     </View>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#00CCFF",
+      },
+      fontFamily: {
+        "Poppins-Black": ["Poppins-Black", "sans-serif"],
+        Poppins: ["Poppins-Regular", "sans-serif"],
+        Roboto: ["Roboto-Regular", "sans-serif"],
+        Merriweather: ["Merriweather-Light", "serif"],
+      },
+    },
   },
   plugins: [],
 };
